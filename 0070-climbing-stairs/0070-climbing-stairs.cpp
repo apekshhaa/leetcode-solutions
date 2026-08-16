566ce74636995d42094be16f7b11e902ed072241
Apeksha S <143615663+apekshhaa@users.noreply.github.com>
2026-08-16 20:07:34 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.69%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int sum = 0;
+        if(n<=2){
+            return n;
+        }
+        int current = 0;
+        int prev1 = 2;
+        int prev2 = 1;
+        for(int i=3;i<=n;i++){
+            current = prev1+prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+};
